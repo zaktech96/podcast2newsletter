4fb63cbdcb4df0e688f54cc8470d6fbb673a899a
zaktech96 <zakariyesahid96@gmail.com>
2025-06-03 15:09:07 +0100

    sign up button routed

--- app/(marketing)/marketing-page/page.tsx
+++ app/(marketing)/marketing-page/page.tsx
@@ -35,13 +35,13 @@ export default async function MarketingPage() {
         </p>
         <div className="flex gap-2 mt-2">
           <Link 
-            href="/dashboard"
+            href="/sign-up"
             className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
           >
             Get Started
           </Link>
           <Link 
-            href="/dashboard" 
+            href="/sign-up" 
             className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-11 px-8 mt-2"
           >
             Get Started
--- components/wrapper/footer.tsx
+++ components/wrapper/footer.tsx
@@ -55,7 +55,7 @@ export const Footer = () => {
       <div className="container px-4 py-8 mx-auto">
         <div className="flex flex-col items-center space-y-4">
           <Link 
-            href="/dashboard"
+            href="/sign-up"
             className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
           >
             Get Started <ArrowRight className="ml-2 h-4 w-4" />
